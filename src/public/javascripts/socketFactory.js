69e55783a0359515bd7e95555e09a57cca05eaa2
Josh Kopman <jkopman@loaner-ny-04031.ad.hugeinc.com>
2014-09-25 15:09:12 -0400

    socket.io

--- src/public/javascripts/socketFactory.js
+++ src/public/javascripts/socketFactory.js
@@ -1,5 +1,5 @@
 app.factory('socket', ['$rootScope', function ($rootScope) {
-  var socket = io.connect('http://10.14.12.150:7075');
+  var socket = io.connect('http://10.14.12.150');
   return {
     on: function (eventName, callback) {
       socket.on(eventName, function () {  
