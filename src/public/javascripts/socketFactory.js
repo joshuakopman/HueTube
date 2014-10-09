050e469cada1fb119aef7a4546e44855503eb8c2
Josh Kopman <jkopman@jkopma-ny-06159.ad.hugeinc.com>
2014-10-09 17:15:48 -0400

    Adding socket port

--- src/public/javascripts/socketFactory.js
+++ src/public/javascripts/socketFactory.js
@@ -1,5 +1,5 @@
 app.factory('socket', ['$rootScope', function ($rootScope) {
-  var socket = io.connect('http://10.14.12.150');
+  var socket = io.connect('http://10.14.12.150:7075');
   return {
     on: function (eventName, callback) {
       socket.on(eventName, function () {  
