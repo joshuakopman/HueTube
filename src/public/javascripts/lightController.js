a2b697a4667a6b9a9472adfceb2e6fbff405ec87
Josh Kopman <jkopman@jkopma-ny-06159.ad.hugeinc.com>
2014-12-04 17:58:48 -0500

    changing server name

--- src/public/javascripts/lightController.js
+++ src/public/javascripts/lightController.js
@@ -41,7 +41,7 @@ $scope.toggle = function(id,state,hue,bri,sat,effect,isGroup){
   var endPoint = (isGroup) ? 'groups/':'lights/';
   
 
-  $http.put('http://10.14.12.150/' + endPoint + id, JSON.stringify(lightStateChange)).
+  $http.put('http://' + window.location.hostname + '/' + endPoint + id, JSON.stringify(lightStateChange)).
       success(function(data) {
           clicked = false;
       });
