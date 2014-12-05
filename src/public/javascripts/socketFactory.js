6f933d6b495a5ab73a05d189b71f75fc975d5a30
Josh Kopman <jkopman@jkopma-ny-06159.ad.hugeinc.com>
2014-12-05 15:05:06 -0500

    port test

--- src/public/javascripts/socketFactory.js
+++ src/public/javascripts/socketFactory.js
@@ -1,5 +1,5 @@
 app.factory('socket', ['$rootScope', function ($rootScope) {
-  var socket = io.connect('http://' + window.location.hostname);
+  var socket = io.connect('http://' + window.location.hostname + ':13243');
   return {
     on: function (eventName, callback) {
       socket.on(eventName, function () {  
