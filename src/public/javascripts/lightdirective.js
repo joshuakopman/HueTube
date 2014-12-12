d89dc9cdb11bf4696b3c558f88923375735048e8
Josh Kopman <jkopman@jkopma-ny-06159.ad.hugeinc.com>
2014-12-12 15:46:47 -0500

    splitting out templates

--- src/public/javascripts/lightdirective.js
+++ src/public/javascripts/lightdirective.js
@@ -5,13 +5,8 @@ angular.module("light",["ng"]).directive("light",function(){
 		restrict:"EA",
 		replace:!0,
 		scope:{model:"=",toggleLight:"="},
-	    template: '<div class="toggle-bg toggle-alternate {{model.State}}">'+
-				      '<label class="{{model.State}}">{{model.State}}</label>'+
-				      '<input type="radio" name="toggle_{{model.lightid}}" ng-click="toggleLight(model.lightid,model.State)" value="off" ng-model="model.State">'+
-				      '<input type="radio" name="toggle_{{model.lightid}}" ng-click="toggleLight(model.lightid,model.State)" value="on" ng-model="model.State">'+
-				     '<span class="switch {{model.State}}"></span>'+
-				  '</div>'
-		}
+	    templateUrl: 'templates/light.html'
+	}
 });
 
 angular.module("wakeup",["ng"]).directive("wakeup",function(){
@@ -19,10 +14,8 @@ angular.module("wakeup",["ng"]).directive("wakeup",function(){
 		restrict:"EA",
 		replace:!0,
 		scope:{wakeAlert:"&"},
-	    template: '<div class="wakeup">'+
-				      '<input type="image" src=".\\images\\sonic.png" title="Send the batsignal" height="35" width="35" name="wakeup" ng-click="wakeAlert()" value="Bat Signal">'+
-				  '</div>'
-		}
+		templateUrl: 'templates/wakeup.html'
+	}
 });
 
 angular.module("colorpicker",["ng"]).directive("colorpicker",function(){
@@ -30,18 +23,7 @@ angular.module("colorpicker",["ng"]).directive("colorpicker",function(){
 		restrict:"EA",
 		replace:!0,
 		scope:{ setColor : "=",lightid : "@" },
-	    template: 
-        '   <div class="colorPanel">'+
-	    	'	<div class="swatch red" ng-click="setColor(lightid,\'off\',0,255,255,\'\')"></div>'+
-	    	'	<div class="swatch orange" ng-click="setColor(lightid,\'off\',9000,255,255,\'\')"></div>'+
-	    	'	<div class="swatch yellow" ng-click="setColor(lightid,\'off\',17750,255,255,\'\')"></div>'+
-	    	'	<div class="swatch green" ng-click="setColor(lightid,\'off\',25718,255,255,\'\')"></div>'+
-	    	'	<div class="swatch blue" ng-click="setColor(lightid,\'off\',46920,255,255,\'\')"></div>'+
-	    	'	<div class="swatch purple" ng-click="setColor(lightid,\'off\',50000,255,255,\'\')"></div>'+
-	    	'	<div class="swatch white" ng-click="setColor(lightid,\'off\',0,255,0,\'\')"></div>'+
-   		' 	</div> 	'
-
-   				}
-
+	    templateUrl: 'templates/colorpicker.html'
+    }
 });
 
