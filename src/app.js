570d1b79e8b280c9041bcc1b07ff100c181a2054
Joshua Kopman <joshuakopman@Joshuas-MBP.home>
2015-06-14 00:15:21 -0400

    initial experimentation with node auth

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var LightController = require('./controllers/LightController');
 var LightService = require('./services/LightService');
+var AuthService = require('./services/AuthService');
 var Config = require("./Config")
 var cors = require('cors')
 
@@ -11,6 +12,10 @@ app.use(cors());
 app.use(bodyParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.disable('etag');
+
+app.use(function(req, res, next) {
+	new AuthService().PromptForCredentials(req,res,next);
+});
 
 var server = app.listen(Config.node.port, function() { });
 
@@ -23,6 +28,3 @@ var io = require('socket.io').listen(server);
 io.sockets.on('connection', function (newSocket){
 	new LightController(new LightService()).BuildRouting(app,newSocket);
 });
-
-
-//app.use(require('express.io').basicAuth('testUser', 'testPass'));
