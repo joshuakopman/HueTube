2bf96e2ed74c58c31d05612d0db60e8304310943
Josh Kopman <jkopman@hugeinc.com>
2015-06-22 16:11:13 -0400

    service updates

--- services/SpotifyService.js
+++ services/SpotifyService.js
@@ -21,22 +21,24 @@ SpotifyService.prototype.startAirplay = function(callback){
     this.getJSON(function(statusCode){
 		callback(statusCode);
     });*/
+
+	callback();
 }
 
 SpotifyService.prototype.startSpotify = function(callback){
-	/*var requestOptions = {
-	    host: Config.host,
-	    port: Config.hue.port,
-	    path: Config.hue.uri + '/spotify/',
-	    method: 'POST',
+	var requestOptions = {
+	    host: Config.spotify.host,
+	    port: Config.spotify.port,
+	    path: Config.spotify.uri,
+	    method: 'GET',
 	    headers: {
 	        'Content-Type': 'application/json'
 	    },
     };
     this.options = requestOptions;
-    this.getJSON(function(statusCode){
-		callback(statusCode);
-    });*/
+    this.getJSON(function(statusCode,data){
+    	callback(data);
+    });
 }
 
 module.exports = SpotifyService;
--- services/WemoService.js
+++ services/WemoService.js
@@ -1,29 +1,27 @@
 var WeMo = require('wemo')
 var Config = require("../Config");
 
-
 function WemoService(options){
 	this.options = options;
+	this.wemoSwitch = new WeMo(Config.host, Config.wemo.port);
 };
 
 WemoService.prototype.changeState = function(callback){
-	var wemoSwitch = new WeMo(Config.host, Config.wemo.port);
+	var self = this;
 
-	wemoSwitch.getBinaryState(function(err, result) {
+	this.wemoSwitch.getBinaryState(function(err, result) {
 		var onOff = 0;
 		if(result == 0){
 			onOff = 1;
 		}
-    	wemoSwitch.setBinaryState(onOff, function(err, result) { // switch on 
+    	self.wemoSwitch.setBinaryState(onOff, function(err, result) { // switch on 
 			return callback(result);
 		});
 	});
 }
 
 WemoService.prototype.getState = function(callback){
-	var wemoSwitch = new WeMo(Config.host, Config.wemo.port);
-
-	wemoSwitch.getBinaryState(function(err, result) {
+	this.wemoSwitch.getBinaryState(function(err, result) {
 		var state = '';
 		if(result == 0){
 			state = "off";
@@ -33,5 +31,12 @@ WemoService.prototype.getState = function(callback){
 		callback(state);
 	});
 }
+
+WemoService.prototype.turnOnWemo = function(callback){
+	this.wemoSwitch.setBinaryState(1, function(err, result) { // switch on 
+		return callback(result);
+	});
+}
+
 
 module.exports = WemoService;
