834d1e984c8b506d7150f102ac7325d3e6c54387
Josh Kopman <jkopman@hugeinc.com>
2015-09-14 16:04:13 -0400

    script

--- init.sh
+++ init.sh
@@ -2,6 +2,8 @@ sudo docker stop $(sudo docker ps -aq)
 
 sudo docker rm $(sudo docker ps -aq)
 
+sudo docker rmi $(sudo docker images -q)
+
 sudo docker run -itd -p 27017:27017 --name mongodb mongo
 
 sudo docker run  --link mongodb:mongodb -p 80:7076 -p 7076:7076 -i aesopwaits/huetube
